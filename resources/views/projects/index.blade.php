f7f5f92b918196f376674c27f1a218af042e1a79
Alexander Foster <alexanderfoster@Alexanders-MacBook-Pro.local>
2021-01-25 16:41:08 -0700

    Adding data as well as fixing str::limit error as it is caleld statically in laravel 8

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -5,12 +5,14 @@
         <h1 class="mr-auto">Laravel Job Board</h1>
         <a href="{{route('projects.create')}}">New Project</a>
     </div>
-    <div class="container flex">
+    <div class="container flex flex-wrap -mx-3">
        @forelse($projects as $project)
-            <div class="project mr-4 p-5 rounded shadow bg-white w-1/3" style="height: 200px;">
-                <h3 class="font-normal text-xl py-4">{{$project->title}}</h3>
-                <p class="text-grey">{{ str_limit($project->description, 250) }}</p>
-            </div>
+           <div class="w-1/3 px-3 pb-6">
+               <div class="project p-5 rounded shadow bg-white " style="height: 200px;">
+                   <h3 class="font-normal text-xl py-4">{{$project->title}}</h3>
+                   <p class="text-grey">{{ \Str::limit($project->description, 250) }}</p>
+               </div>
+           </div>
         @empty
            <div>Sorry, there are no projects yet.</div>
         @endforelse
